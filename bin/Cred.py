59661c8174628302ed1156acfe77f8291c44e06c
upekshaip <upekshaip@gmail.com>
2024-03-23 15:08:47 +0530

    updated

--- bin/Cred.py
+++ bin/Cred.py
@@ -1,4 +1,3 @@
-# Telegram API
 class Cred:
     
     FIREBASE_CONF = {
@@ -10,11 +9,3 @@ class Cred:
         'appId': "1:93442721481:web:d80fbcdbf12ca9a8cb6f18",
         'databaseURL': "https://medsync-af55a-default-rtdb.asia-southeast1.firebasedatabase.app/"
     }
-
-# apiKey: "AIzaSyACS41aaG0cKiSAUP8JXf5j5nh_4lN1QbM",
-#   authDomain: "medsync-af55a.firebaseapp.com",
-#   databaseURL: "https://medsync-af55a-default-rtdb.asia-southeast1.firebasedatabase.app",
-#   projectId: "medsync-af55a",
-#   storageBucket: "medsync-af55a.appspot.com",
-#   messagingSenderId: "93442721481",
-#   appId: "1:93442721481:web:d80fbcdbf12ca9a8cb6f18"
