cd1410fef8bb1136a0e7169b769d4b0fcf22d0b5
Upeksha Indeewara <102838768+upekshaip@users.noreply.github.com>
2024-07-17 22:57:47 +0530

    Update Cred.py

--- bin/Cred.py
+++ bin/Cred.py
@@ -1,10 +1,4 @@
 class Cred:
     FIREBASE_CONF = {
-        'apiKey': "AIzaSyCXqFAH0pu-fyVqiIZz9BDhzSh3FJMpWhs",
-        'authDomain': "medsync-c1dbc.firebaseapp.com",
-        'projectId': "medsync-c1dbc",
-        'storageBucket': "medsync-c1dbc.appspot.com",
-        'messagingSenderId': "232270548251",
-        'appId': "1:232270548251:web:7ea59699553e992c8917be",
-        'databaseURL': "https://medsync-c1dbc-default-rtdb.asia-southeast1.firebasedatabase.app/"
+       
     }
